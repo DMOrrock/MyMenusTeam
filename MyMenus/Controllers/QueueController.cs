ae91bc47200b5800cf695a0b6de0bf86d2bb663a
MyMenus <dorrock3@byu.edu>
2019-10-14 11:44:17 -0600

    First commit

---
+++ MyMenus/Controllers/QueueController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMenus.Controllers
+{
+    public class QueueController : Controller
+    {
+        static Queue<string> myQueue = new Queue<string>();
+
+        // GET: Queue
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
+            ViewBag.queueIndex = "Added One";
+
+            return View("Index");
+        }
+
+        public ActionResult AddMany()
+        {
+            myQueue.Clear();
+            for (int i = 0; i < 2000; i++)
+            {
+                myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
+            }
+            ViewBag.queueIndex = "Added Many";
+
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+            ViewBag.queueDisplay = myQueue;
+            return View();
+        }
+
+        public ActionResult DeleteOneQueue()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteItemQueue(string place)
+        {
+            Queue<string> tempQueue = new Queue<string>();
+
+            if (myQueue.Contains(place))
+            {
+                while (myQueue.Count > 0)
+                {
+                    if (myQueue.Peek() == place)
+                    {
+                        myQueue.Dequeue();
+                    }
+                    else
+                    {
+                        tempQueue.Enqueue(myQueue.Dequeue());
+                    }
+                }
+                while (tempQueue.Count > 0)
+                {
+                    myQueue.Enqueue(tempQueue.Dequeue());
+                }
+                ViewBag.queueIndex = "Item found and deleted";
+            }
+            else
+            {
+                ViewBag.queueIndex = "Item not found in Queue";
+            }
+
+
+            return View("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            myQueue.Clear();
+            ViewBag.queueIndex = "Cleared";
+
+            return View("Index");
+        }
+
+        public ActionResult SearchForItemQueue()
+        {
+            return View();
+        }
+
+        public ActionResult SearchItemQueue(string place)
+        {
+            Queue<string> tempQueue = new Queue<string>();
+            int positionInQueue = 0;
+            int iCounter = 0;
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            if (myQueue.Contains(place))
+            {
+                sw.Start();
+
+                while (myQueue.Count > 0)
+                {
+                    iCounter += 1;
+                    if (myQueue.Peek() == place)
+                    {
+                        positionInQueue = iCounter;
+                        tempQueue.Enqueue(myQueue.Dequeue());   
+                    }
+                    else
+                    {
+                        tempQueue.Enqueue(myQueue.Dequeue());
+                    }
+                }
+                sw.Stop();
+                TimeSpan ts = sw.Elapsed;
+                ViewBag.queueIndex = "Item found | Index at: " + positionInQueue + " | Time elapsed: " + ts;
+
+                while (tempQueue.Count > 0)
+                {
+                    myQueue.Enqueue(tempQueue.Dequeue());
+                }
+            }
+            else
+            {
+                ViewBag.queueIndex = "Item not found in Queue";
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ MyMenus/Controllers/StackController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMenus.Controllers
+{
+    public class StackController : Controller
+    {
+        static Stack<string> myStack = new Stack<string>();
+
+        // GET: Stack
+        public ActionResult Index()
+        {
+            ViewBag.IndexBag = "Welcome to the Stack Page";
+            return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            myStack.Push("New Entry " + (myStack.Count + 1));
+            //ViewBag.StackBag = myStack;
+            ViewBag.IndexBag = "Added One";
+            return View("Index");
+        }
+
+        public ActionResult AddMany()
+        {
+            myStack.Clear();
+            for (int i = 0; i < 2000; i++)
+            {
+                myStack.Push("New Entry " + (myStack.Count + 1));
+                //ViewBag.StackBag = myStack;
+            }
+            ViewBag.IndexBag = "Added Many";
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+            ViewBag.StackBag = myStack;
+            return View("Display");
+        }
+
+        public ActionResult DeleteOne()
+        {
+            return View("DeleteOne");
+        }
+
+        public ActionResult DeleteItem(string place)
+        {
+            Stack<string> tempStack = new Stack<string>();
+
+            if (myStack.Contains(place) == true)
+            {
+                while (myStack.Count > 0)
+                {
+                    if (place == myStack.Peek())
+                    {
+                        myStack.Pop();
+                        break;
+                    }
+                    else
+                    {
+                        tempStack.Push(myStack.Pop());
+                    }
+                }
+
+                while (tempStack.Count > 0)
+                {
+                    myStack.Push(tempStack.Pop());
+                }
+                ViewBag.IndexBag = "Item found and deleted";
+            }
+
+            else
+            {
+                ViewBag.IndexBag = "Item not in Stack";
+            }
+            return View("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            myStack.Clear();
+            ViewBag.IndexBag = "Cleared";
+            return View("Index");
+        }
+
+        public ActionResult SearchForItem()
+        {
+            return View("SearchForItem");
+        }
+
+        public ActionResult Search(string place)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            int positionInStack = 0;
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            if (myStack.Contains(place) == true)
+            {
+                sw.Start();
+
+                while (myStack.Count > 0)
+                {
+                    positionInStack += 1;
+                    if (myStack.Peek() == place)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        tempStack.Push(myStack.Pop());
+                    }
+                }
+                sw.Stop();
+                TimeSpan ts = sw.Elapsed;
+                ViewBag.IndexBag = "Item found | Index at: " + positionInStack + " | Time elapsed: " + ts;
+
+                while (tempStack.Count > 0)
+                {
+                    myStack.Push(tempStack.Pop());
+                }
+            }
+            else
+            {
+                ViewBag.IndexBag = "Item not in Stack";
+            }
+            return View("Index");
+        }
+    }
+}
